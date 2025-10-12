bdb7b0855d15cfba9a2217707696880e2dbad9f7
Luiz Carlos Martendal <luiz.c.martendal@hotmail.com>
2025-10-12 19:35:14 -0300

    add classe modelo e usando arrays

---
+++ conceitos-basicos/src/app/lista-compras/itemLista.ts
@@ -0,0 +1,5 @@
+export class ItemLista{
+    nome?: string;
+    id?: number;
+    comprado: boolean = false;
+}
--- conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemLista'
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [ FormsModule ],
+  imports: [ FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
-  item: string = ""
+  item: string = "";
+  lista: ItemLista[] = [];
 
   adicionarItem(){
-    console.log("Item recebido: " + this.item);
+    let itemLista = new ItemLista();
+
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length +1;
+
+    this.lista.push(itemLista);
+
     this.item = '';
+
+    console.table(this.lista);
   }
 }
