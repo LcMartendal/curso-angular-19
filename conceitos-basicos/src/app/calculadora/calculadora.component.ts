201bae7ec620001380e2745f030b17c03a2f03c3
Luiz Carlos Martendal <luiz.c.martendal@hotmail.com>
2025-10-11 21:00:14 -0300

    add ações ao component

--- conceitos-basicos/src/app/calculadora/calculadora.component.ts
+++ conceitos-basicos/src/app/calculadora/calculadora.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.scss'
 })
 export class CalculadoraComponent {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
 
+  calcularResultado(){
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
